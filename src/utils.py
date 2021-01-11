cada02f605e59ab741cfd71ce9719fc6fd8f31d9
erwan.vivien <erwan.vivien@epita.fr>
2021-01-11 14:26:02 +0100

    added get request

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,14 @@
+import requests
+
+
 def get_content(file):
     # Read file content
     file = open(file, "r")
     s = file.read()
     file.close()
     return s
+
+
+def subreddit_json(subreddit):
+    r = requests.get(f"http://reddit.com/r/{subreddit}.json&limit=100")
+    return r.json()
